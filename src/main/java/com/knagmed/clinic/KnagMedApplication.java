c5c99e1b00d3e07a4e16153d1c73d9dc96333b97
Adrian Rubak <adek.rubak@gmail.com>
2022-03-17 13:59:50 +0100

    Initial commit

---
+++ src/main/java/com/knagmed/clinic/KnagMedApplication.java
@@ -0,0 +1,13 @@
+package com.knagmed.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnagMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnagMedApplication.class, args);
+	}
+
+}
