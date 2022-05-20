a3da7f8668c9d4a5e5d55e123dc99397d28253f2
Adrian Rubak <adek.rubak@gmail.com>
2022-05-20 22:26:30 +0200

    fix

--- src/main/java/com/knagmed/clinic/security/controller/AuthenticationController.java
+++ src/main/java/com/knagmed/clinic/security/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.knagmed.clinic.security.controller;
 
-import com.knagmed.clinic.security.auth.AppUserDetailsService;
+import com.knagmed.clinic.security.auth.AppUser;
+import com.knagmed.clinic.security.auth.AppUserDetails;
+import com.knagmed.clinic.security.auth.AppUserRepository;
 import com.knagmed.clinic.security.jwt.JwtUtil;
 import com.knagmed.clinic.security.jwt.request.AuthenticationRequest;
 import com.knagmed.clinic.security.jwt.request.AuthenticationResponse;
@@ -9,8 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -19,25 +23,25 @@ public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
-    private final AppUserDetailsService appUserService;
+    private final AppUserRepository appUserRepository;
 
     @PostMapping
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            authenticationRequest.getUsername(),
-                            authenticationRequest.getPassword()
-                    )
+                new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+                )
             );
-        }catch (BadCredentialsException e) {
+        } catch (BadCredentialsException e) {
             throw new Exception("Incorrect username or password", e);
         }
 
-        final UserDetails userDetails = appUserService.loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        final AppUser appUser = appUserRepository.findByUsername(authenticationRequest.getUsername()).get();
+        final String jwt = jwtUtil.generateToken(new AppUserDetails(appUser));
+        return ResponseEntity.ok(new AuthenticationResponse(appUser.getId(), jwt));
     }
 
 }
--- src/main/java/com/knagmed/clinic/security/jwt/request/AuthenticationResponse.java
+++ src/main/java/com/knagmed/clinic/security/jwt/request/AuthenticationResponse.java
@@ -8,8 +8,6 @@ import java.io.Serializable;
 @Getter
 @AllArgsConstructor
 public class AuthenticationResponse implements Serializable {
-
+    private final Long userId;
     private final String jwt;
-
-
 }
